04b73fc0fde232a9b5a28a3a2287c8c7b0569e45
elisabethkuning <ekuning@tktk.ee>
2023-02-17 14:15:10 +0200

    add week4 code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿Random rnd = new Random();
+
+
+//int myRandomNum = rnd.Next(0, 11); // genereeritakse väärtused 0-10
+//Console.WriteLine($"My random number is: {myRandomNum}");
+
+int myRandomNum;
+int myRandomSum = 0;
+
+for(int i = 0; i < 5; i++)
+{
+    myRandomNum = rnd.Next(0, 11); // genereeritakse väärtused 0-10
+    myRandomSum = myRandomSum + myRandomNum;
+    Console.WriteLine($"My random number is: {myRandomNum}");
+}
+
+Console.WriteLine($"Ramdom sum total: {myRandomSum}");
